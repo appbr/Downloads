e399bf55d2729694d2f4da691eb4fe821bfaa922
appbr <telles.jadlog@gmail.com>
2022-11-18 13:42:07 -0300

    desinteligencia Artificial

--- script/script1.js
+++ script/script1.js
@@ -1,12 +1,15 @@
 
+
 var jogador = 'x';
 var jogada = 0;
 
 function verificarJogo(id) {
+    var cpu = document.getElementById('cpu').checked
+
 
 
     var enderoSRC_Resumido = enderecoSRC_Resumido(id);
-    
+
     if (enderoSRC_Resumido == 'images/branco.png') {
         document.getElementById(id).src = "images/" + jogador + ".png";
         jogada++;
@@ -17,18 +20,18 @@ function verificarJogo(id) {
             msg.innerHTML = `O vencedor é o jogador (${jogador})`
             enderecoSRC_Resumido = "images/branco.png"
 
-           
-            
+
+
         }
 
-        if(jogada >=9){
+        if (jogada >= 9) {
             var falha = document.querySelector('#falha')
             falha.play();
             msg.hidden = false;
             msg.innerHTML = `EMPATE!!!`
             enderecoSRC_Resumido = "images/branco.png"
         }
-        
+
 
         if (jogador == 'x') {
             jogador = 'o'
@@ -43,6 +46,13 @@ function verificarJogo(id) {
 
 
     }
+    if (cpu && jogador == "o") {
+        setTimeout(() => {
+            verificarJogo(jogadaComputador());
+        }, 1000);
+
+    }
+
 
 }
 
@@ -100,6 +110,75 @@ function verificarVitoria() {
     return false;
 }
 
-function empate(){
+
+
+function jogadaComputador() {
+
+    if(enderecoSRC_Resumido('c5') == 'images/branco.png'){return 'c5'}
+
+    if(enderecoSRC_Resumido('c1') == enderecoSRC_Resumido('c2') && enderecoSRC_Resumido('c3') == 'images/branco.png'){
+        return 'c3'
+    }
+
+    if(enderecoSRC_Resumido('c1') == enderecoSRC_Resumido('c3') && enderecoSRC_Resumido('c2') == 'images/branco.png'){
+        return 'c2'
+    }
+
+    if(enderecoSRC_Resumido('c4') == enderecoSRC_Resumido('c5') && enderecoSRC_Resumido('c6') == 'images/branco.png'){
+        return 'c6'
+    }
     
+    if(enderecoSRC_Resumido('c4') == enderecoSRC_Resumido('c6') && enderecoSRC_Resumido('c5') == 'images/branco.png'){
+        return 'c5'
+    }
+
+    if(enderecoSRC_Resumido('c7') == enderecoSRC_Resumido('c8') && enderecoSRC_Resumido('c9') == 'images/branco.png'){
+        return 'c9'
+    }
+
+    if(enderecoSRC_Resumido('c7') == enderecoSRC_Resumido('c9') && enderecoSRC_Resumido('c8') == 'images/branco.png'){
+        return 'c8'
+    }
+
+    if(enderecoSRC_Resumido('c1') == enderecoSRC_Resumido('c4') && enderecoSRC_Resumido('c7') == 'images/branco.png'){
+        return 'c7'
+    }
+
+    if(enderecoSRC_Resumido('c1') == enderecoSRC_Resumido('c7') && enderecoSRC_Resumido('c4') == 'images/branco.png'){
+        return 'c4'
+    }
+
+    if(enderecoSRC_Resumido('c2') == enderecoSRC_Resumido('c5') && enderecoSRC_Resumido('c8') == 'images/branco.png'){
+        return 'c8'
+    }
+
+    if(enderecoSRC_Resumido('c2') == enderecoSRC_Resumido('c8') && enderecoSRC_Resumido('c5') == 'images/branco.png'){
+        return 'c5'
+    }
+
+    if(enderecoSRC_Resumido('c3') == enderecoSRC_Resumido('c6') && enderecoSRC_Resumido('c9') == 'images/branco.png'){
+        return 'c9'
+    }
+
+    if(enderecoSRC_Resumido('c3') == enderecoSRC_Resumido('c9') && enderecoSRC_Resumido('c6') == 'images/branco.png'){
+        return 'c6'
+    }
+
+    if(enderecoSRC_Resumido('c1') == enderecoSRC_Resumido('c5') && enderecoSRC_Resumido('c9') == 'images/branco.png'){
+        return 'c9'
+    }
+
+    if(enderecoSRC_Resumido('c1') == enderecoSRC_Resumido('c9') && enderecoSRC_Resumido('c5') == 'images/branco.png'){
+        return 'c5'
+    }
+
+    if(enderecoSRC_Resumido('c3') == enderecoSRC_Resumido('c5') && enderecoSRC_Resumido('c7') == 'images/branco.png'){
+        return 'c7'
+    }
+
+    if(enderecoSRC_Resumido('c3') == enderecoSRC_Resumido('c7') && enderecoSRC_Resumido('c5') == 'images/branco.png'){
+        return 'c5'
+    }
+
+    return 'c' + Math.floor((Math.random() * 9) + 1);
 }
